9833d3f01c4f3a471a1fc75b7b675f06b203675b
TheKaizenWolf <raul670saavedra@gmail.com>
2020-01-23 13:07:28 -0300

    update text

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const About = () => (
     <Title className="tc-m">About me...!</Title>
     <p className="f4 fw3 tc lh-copy">
       Hello! My name is Raul and I am a software engineer currently living in
-      Saint Louis, MO. I love to code, to travel, and to pet dogs. <br />I
+      Chile. I love to code, to travel, and to pet dogs. <br />I
       follow the Kaizen philosophy, which translates to continuous improvement
       everyday.
     </p>
