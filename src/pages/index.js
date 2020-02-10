93c6e54fe327e0f2e2da0b0cc7bce8d2a56c7592
TheKaizenWolf <raul670saavedra@gmail.com>
2020-02-09 23:23:09 -0300

    update text

--- src/pages/index.js
+++ src/pages/index.js
@@ -254,11 +254,9 @@ const Index = () => {
           <TechnologiesP>
             I am a <a className="light-blue">JAMStack specialist</a> with a big
             passion for delivering excellent static and dynamic user
-            experiences. Including{' '}
-            <a className="light-green">
-              integrations with, Cloud CMS, lambda functions
-            </a>
-            , whatever your needs, I will bring a{' '}
+            experiences. Including integrations with{' '}
+            <a className="light-green">Cloud CMS, lambda functions</a>, whatever
+            your needs, I will bring a{' '}
             <a className="light-green">complete, secure and great performing</a>{' '}
             product.
           </TechnologiesP>
