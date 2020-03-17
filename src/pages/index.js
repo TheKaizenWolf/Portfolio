ade2ec84a9822e9be42a13d6238cf97f78ae83a6
TheKaizenWolf <raul670saavedra@gmail.com>
2020-03-17 15:02:50 -0300

    fix unused import index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import Layout from '../components/layout';
 import Hero from '../components/HomePage/hero';
 import Services from '../components/HomePage/services';
