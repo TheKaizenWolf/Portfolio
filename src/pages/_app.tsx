8b9fff171a35798476091211acee8987e735dab9
raulsaavedra <raul670saavedra@gmail.com>
2021-11-05 10:28:40 -0300

    fix: ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 
 import Head from 'next/head';
-import { globalCss } from '../stitches';
+import { getCssText, globalCss } from '../stitches';
 
 const globalStyles = globalCss({
   '*': {
@@ -39,6 +39,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
+        />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
       </Head>
       <Component {...pageProps} />
--- src/stitches.tsx
+++ src/stitches.tsx
@@ -1,38 +1,40 @@
 import { createStitches } from '@stitches/react';
 
-export const { styled, css, keyframes, globalCss } = createStitches({
-  theme: {
-    colors: {
-      redMedium: 'hsl(338, 100%, 60%)',
-      redDark: 'hsl(338, 100%, 44%)',
-      grayLight: 'hsl(226, 70%, 90%)',
-      grayMedium: 'hsl(225, 20%, 70%)',
-      grayDarker: 'hsl(218, 38%, 15%)',
-      grayDarkest: 'hsl(216, 65%, 9%)',
+export const { styled, css, keyframes, globalCss, getCssText } = createStitches(
+  {
+    theme: {
+      colors: {
+        redMedium: 'hsl(338, 100%, 60%)',
+        redDark: 'hsl(338, 100%, 44%)',
+        grayLight: 'hsl(226, 70%, 90%)',
+        grayMedium: 'hsl(225, 20%, 70%)',
+        grayDarker: 'hsl(218, 38%, 15%)',
+        grayDarkest: 'hsl(216, 65%, 9%)',
+      },
+      fontSizes: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        md: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '3.75rem',
+        '7xl': '4.5rem',
+        '8xl': '6rem',
+        '9xl': '8rem',
+      },
+      sizes: {
+        small: '640px',
+        medium: '1020px',
+        large: '1200px',
+        extraLarge: '1440px',
+      },
     },
-    fontSizes: {
-      xs: '0.75rem',
-      sm: '0.875rem',
-      md: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '3.75rem',
-      '7xl': '4.5rem',
-      '8xl': '6rem',
-      '9xl': '8rem',
+    media: {
+      bpLg: '(max-width: 1024px)',
     },
-    sizes: {
-      small: '640px',
-      medium: '1020px',
-      large: '1200px',
-      extraLarge: '1440px',
-    },
-  },
-  media: {
-    bpLg: '(max-width: 1024px)',
-  },
-});
+  }
+);
